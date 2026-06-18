eb4986d181f3cd5f95d8cd5fb12be3f46c6c5bca
Carlinbf <carlosbifesf@gmail.com>
2026-06-18 13:37:18 -0300

    Bomba.cpp

---
+++ dispositivo_cpp/src/Bomba.cpp
@@ -0,0 +1,12 @@
+#include "../include/Bomba.hpp"
+
+Bomba::Bomba(std::string idBomba, double vazao)
+    : id(idBomba), ligada(false), vazaoNominal(vazao) {}
+
+void Bomba::ligar() {
+    ligada = true;
+}
+
+void Bomba::desligar() {
+    ligada = false;
+}
